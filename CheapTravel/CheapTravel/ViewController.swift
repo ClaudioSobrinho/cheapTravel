6dfe5a988dd281eb1c7d51d8e5c417ecfb08d4b4
Claudio Sobrinho <sobrinho.cal@gmail.com>
2019-01-15 21:05:05 +0000

    First commit, added json and basic folder structure for mvvm

---
+++ CheapTravel/CheapTravel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CheapTravel
+//
+//  Created by Claudio Sobrinho on 1/15/19.
+//  Copyright © 2019 Claudio Sobrinho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
